a5e971e274a1692e8dee40339276e207a001bc0d
Njatva143 <njatva143@gmail.com>
2025-04-18 07:36:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import Login from './pages/login';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
