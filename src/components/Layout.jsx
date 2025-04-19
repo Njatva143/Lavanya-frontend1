6d1526e269d7cea693cebf89655035a2b543d260
Njatva143 <njatva143@gmail.com>
2025-04-19 10:23:09 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,26 @@
-// src/components/Layout.jsx
-import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Employees from '../pages/Employees';
+import SalarySlips from '../pages/SalarySlips';
+import Invoices from '../pages/Invoices';
+import Payments from '../pages/Payments';
 
-const Layout = () => (
-  <div className="flex">
-    <Sidebar />
-    <main className="flex-1 p-4">
-      <Outlet />
-    </main>
-  </div>
-);
+const Layout = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 p-4">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/salary-slips" element={<SalarySlips />} />
+          <Route path="/invoices" element={<Invoices />} />
+          <Route path="/payments" element={<Payments />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
