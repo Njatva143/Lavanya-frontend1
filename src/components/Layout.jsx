530aa0657ae6de00165caffa701468d382796f1c
Njatva143 <njatva143@gmail.com>
2025-04-19 19:41:13 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
-      <main className="flex-1">
+      <div className="flex-1 p-4">
         <Outlet />
-      </main>
+      </div>
     </div>
   );
 };
