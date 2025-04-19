016d4c7bddda24c4454d9ba927c903b9f4ec4d72
Njatva143 <njatva143@gmail.com>
2025-04-19 11:06:51 +0530

    Update DashboardLayout.jsx

--- src/Pages/DashboardLayout.jsx
+++ src/Pages/DashboardLayout.jsx
@@ -1,23 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import Dashboard from '../pages/Dashboard';
-import Employees from '../pages/Employees';
-import SalarySlips from '../pages/SalarySlips';
-import Invoices from '../pages/Invoices';
-import Payments from '../pages/Payments';
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 p-4">
-        <Routes>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="employees" element={<Employees />} />
-          <Route path="salary-slips" element={<SalarySlips />} />
-          <Route path="invoices" element={<Invoices />} />
-          <Route path="payments" element={<Payments />} />
-        </Routes>
+        <Outlet />
       </div>
     </div>
   );
