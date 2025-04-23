f238019b07771b24905ef7d0e2c632d9b03e78ac
Njatva143 <njatva143@gmail.com>
2025-04-23 23:17:40 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
-
+localStorage.setItem("token", "dummy_token");
+navigate("/dashboard");
   const handleSubmit = async (e) => {
     e.preventDefault();
     // After successful login
