1fedf2bdc9d489e28c9cbc85c7d0f3f9cd9a5bfa
Njatva143 <njatva143@gmail.com>
2025-05-05 18:53:54 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import React from "react";
-import { Routes, Route, Navigate }
-from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/
-Dashboard";
-import Layout from 11 ./components/
-Layout";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
 
 const App = () => {
-  const isLoggedIn Boolean (localStor age.getItem("token")); // Convert to
-boolean
-  
-return (
-  <Routes>
-   <Route path="/login"
-element={<Login />} />
-{isLoggedIn ? (
-     <Route path="/"
-element={<Layout />}>
-     <Route index
-element={<Dashboard />} />
-   <Route path="dashboard"
-element={<Dashboard />} /> 
-		 </Route> 
-):( 
-		<Route path="*"
-element={<Navigate to="/login" />} /> )}
+  const isLoggedIn = localStorage.getItem("token");
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/*"
+        element={isLoggedIn ? <Layout /> : <Navigate to="/login" />}
+      >
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
