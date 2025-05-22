f7a6effdc95609fafabf5bf7709d567120d11d12
Njatva143 <njatva143@gmail.com>
2025-05-22 07:42:25 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Layout from "./components/Layout";
-
-const App = () => {
-  const isLoggedIn = localStorage.getItem("token");
-
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/*"
-        element={isLoggedIn ? <Layout /> : <Navigate to="/login" />}
-      >
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
-  );
-};
-
+import React from 'react';
+const App = () => <h1>Welcome to Lavanya Frontend</h1>;
 export default App;
